63e7207ed1ee62e066701e9c32ba929eb31ac6e6
Atlantyspy <94404630+Atlantyspy@users.noreply.github.com>
2025-02-15 11:01:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,30 +6,31 @@ import time
 limit = 3
 # Seuil de détection !!! Varie très fortement en fonction des micros, il faudra passer du temps à essayer de trouver la bonne valeur
 delay = 0.05
-# Delai utilisé pour séquencer le son
+# Delay utilisé pour séquencer le son
 frequence = 44100
 # "Combien de fois par seconde on prend une nouvelle mesure"
 
 compteur = 0
 
-def sound(indata, frames, times, status):    
+def sound(indata, frames, times, status):
+    # indata, données fournies par le module : signal audio sous forme de nombre
+    # frames, times, status sont aussi des données fournies par le module "sounddevice"
+    # frames : nombre d'échantillions audio reçus
+    # times : informations sur le timing des echantillions
+    # status : informe sur la présence d'erreurs     
     global compteur
     sound_level = numpy.linalg.norm(indata)
-    # "indata" : tableau numpy contenant les echantillons audios captés par le micro
-    # chaque valeur représente une intensité sonore
-    # tableau de forme (frames, channels), où frames est le nombre d’échantillons et 
-    # channels est le nombre de canaux (1 pour mono, 2 pour stéréo...).
+    # calcul de la norme : intensité sonore 
+    
     if sound_level > limit :
         compteur +=1
         print(f"Un tir a été détecté : {compteur} tirs")
         time.sleep(0.2)
-        # Ici j'ai mis un timer car j'avais un problème, dès que le programme entendait un son il incrémentait le compteur sauf que si le son durait un peu 
-        # dans le temps, il ajoutait plusieurs valeurs au compteur
         
 with sounddevice.InputStream(callback=sound, samplerate=frequence, channels=1):
-    # "with sounddevice" permet d'ouvrir son micro et de capturer jusqu'à ce que l'onn sorte du with
-    # callback appele la fonction sound() que j'ai crée plut^t et qui permet de capturer le son, de le transformer et de l'analyser
-    # sampelrate= permet de définir la fréquence de capture
+    # "with sounddevice" permet d'ouvrir son micro et de capturer jusqu'à ce que l'on sorte du with
+    # callback appele la fonction sound() que j'ai crée plutôt et qui permet de capturer le son, de le transformer et de l'analyser
+    # samplerate= permet de définir la fréquence de capture
     # channel= permet de sélectionner un mode mono ou stéréo
     
     print("Ecoute en cours...")
